31bcbf84a287333c8fb803ac02ea1ccdb967209f
Leoteixeira3010 <leonardoteixeiraesilva@gmail.com>
2025-07-17 10:40:47 -0300

    📦 Projeto jwt-inspector - versão inicial

---
+++ README.md
@@ -0,0 +1,10 @@
+﻿# JWT Inspector
+
+Ferramenta para análise e manipulação de tokens JWT.
+
+## Funcionalidades:
+- Decodificação de JWT
+- Validação de assinatura (HS256)
+- Análise de payloads inseguros
+
+Stack: Python + PyJWT + Flask
---
+++ main.py
@@ -0,0 +1,17 @@
+﻿from flask import Flask, request, jsonify
+import jwt
+
+app = Flask(__name__)
+SECRET_KEY = "segredo-teste"
+
+@app.route('/decode', methods=['POST'])
+def decode_jwt():
+    token = request.json.get('token')
+    try:
+        decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return jsonify(decoded), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(debug=True)
